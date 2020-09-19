397e48765e309016d65ce744caac75522eb8b174
OpenSiege <opensiege@gmail.com>
2020-09-19 17:10:12 -0600

    added initial loading of the siege nodes database for filename mappings

--- src/state/test/RegionTestState.cpp
+++ src/state/test/RegionTestState.cpp
@@ -11,14 +11,37 @@ namespace ehb
     void RegionTestState::enter()
     {
         auto log = spdlog::get("log");
+        log->set_level(spdlog::level::debug);
+
         log->info("RegionTestState::enter()");
 
-        static const std::string directory = "/world/global/siege_nodes";
-
-        fileSys.eachGasFile(directory, [this, log](const std::string& filename, auto doc)
+        // the first step is to map all the siege node file names to their corresponding guid
         {
-            log->info("reading gas file");
-        });
+            // [mesh_file*] { filename = t_cry01_cave-lc; guid=0xa2010103; }
+            static const std::string directory = "/world/global/siege_nodes"; int32_t fileCount = 0;
+            fileSys.eachGasFile(directory, [this, &fileCount, log](const std::string& filename, auto doc)
+                {
+                    log->debug("loading {}", filename);
+
+                    // first set of children is going to be [t:siege_nodes,n:*]
+                    for (auto root : doc->eachChild())
+                    {
+                        for (auto node : root->eachChild())
+                        {
+                            const auto itr = meshFileNameToGuidKeyMap.emplace(node->valueOf("guid"), osgDB::convertToLowerCase(node->valueOf("filename")));
+
+                            if (itr.second != true)
+                            {
+                                log->debug("duplicate mesh mapping found: tried to insert filename {} for guid {}, but found filename {} there already", node->valueOf("filename"), node->valueOf("guid"), itr.first->second);
+                            }
+                        }
+                    }
+
+                    fileCount++;
+                });
+
+            log->debug("after loading {} files there are {} entries in our mesh filename to key map", fileCount, meshFileNameToGuidKeyMap.size());
+        }
     }
 
     void RegionTestState::leave()
--- src/state/test/RegionTestState.hpp
+++ src/state/test/RegionTestState.hpp
@@ -3,6 +3,8 @@
 
 #include "state/IGameState.hpp"
 
+#include <unordered_map>
+
 namespace osgViewer
 {
     class Viewer;
@@ -33,9 +35,23 @@ namespace ehb
 
         osgViewer::Viewer& viewer;
         osg::Group& scene;
+        
+        // filename=t_cry01_cave-1c; guid=0xa2010103
+        std::unordered_map<std::string, std::string> meshFileNameToGuidKeyMap;
+
+    private:
+
+        const std::string& resolveFileName(const std::string& filename) const;
     };
 
     inline RegionTestState::RegionTestState(IGameStateMgr& gameStateMgr, IConfig& config, IFileSys& fileSys, osgViewer::Viewer& viewer, osg::Group& scene) : gameStateMgr(gameStateMgr), config(config), fileSys(fileSys), scene(scene), viewer(viewer)
     {
+    }
+
+    inline const std::string& RegionTestState::resolveFileName(const std::string& filename) const
+    {
+        const auto itr = meshFileNameToGuidKeyMap.find(filename);
+
+        return itr != meshFileNameToGuidKeyMap.end() ? itr->second : filename;
     }
 }
