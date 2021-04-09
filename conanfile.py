e1944d266e5bba6907b417e38e5d1faa2bb9f644
OpenSiege <opensiege@gmail.com>
2021-04-09 17:34:23 -0600

    added conan to facilitate easier cross-platform development

---
+++ conanfile.py
@@ -0,0 +1,13 @@
+from conans import ConanFile, CMake
+
+class OpenSiegeConan(ConanFile):
+    settings = "os", "compiler", "build_type", "arch"
+    requires = "openscenegraph/3.6.5", "re2c/2.0.3"
+    generators = "cmake"
+    default_options = "openscenegraph:shared=True"
+    
+    def configure(self):
+        if self.settings.os == "Windows":
+            self.requires ("winflexbison/2.5.24")
+        if self.settings.os == "Linux":
+            self.requires ("bison/3.7.1")
