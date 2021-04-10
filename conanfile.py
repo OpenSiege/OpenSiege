cc70dc657875e848df2375d621b7ec76da78d574
OpenSiege <opensiege@gmail.com>
2021-04-09 21:45:17 -0600

    added back re2c and bison with an option to regenerate the gas parser if needs - off by default

--- conanfile.py
+++ conanfile.py
@@ -2,9 +2,15 @@ from conans import ConanFile, CMake
 
 class OpenSiegeConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    requires = "openscenegraph/3.6.5", "spdlog/1.8.5"
+    requires = "openscenegraph/3.6.5", "re2c/2.0.3", "spdlog/1.8.5"
     generators = "cmake"
     default_options = "openscenegraph:shared=True"
+    
+    def configure(self):
+        if self.settings.os == "Windows":
+            self.requires("winflexbison/2.5.24")
+        if self.settings.os == "Linux":
+            self.requires("bison/3.7.1")
     
     def imports(self):
         self.copy("*.dll", dst="bin", src="bin")
