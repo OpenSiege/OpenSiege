27118c8955bd4426dc8ef9dab767e0a2695df8ac
OpenSiege <opensiege@gmail.com>
2021-04-13 04:01:11 -0600

    working on the unveiling state some more

--- src/state/test/UnveilingTestState.cpp
+++ src/state/test/UnveilingTestState.cpp
@@ -12,7 +12,6 @@
 #include <set>
 #include <osgDB/ReadFile>
 #include <osg/Group>
-#include <osg/MatrixTransform>
 #include <osgDB/Options>
 #include <osg/ComputeBoundsVisitor>
 #include <osgViewer/Viewer>
@@ -93,6 +92,101 @@ namespace ehb
         }
     };
 
+    Player::Player(ContentDb& contentDb, osgViewer::Viewer& viewer)
+    {
+        auto log = spdlog::get("log");
+
+        isFidgeting = true;
+        fidgetAnimation = nullptr;
+        walkAnimation = nullptr;
+        animationManager = nullptr;
+
+        if (auto go = contentDb.getGameObjectTmpl(tmplName))
+        {
+            auto model = go->valueOf("aspect:model");
+            const auto chore_prefix = go->valueOf("body:chore_dictionary:chore_prefix");
+            const auto chore_fidget = go->valueOf("body:chore_dictionary:chore_fidget:anim_files:00");
+            auto chore_walk = go->valueOf("body:chore_dictionary:chore_walk:anim_files:00");
+
+            // well this is a silly workaround
+            if (chore_walk.empty()) chore_walk = go->valueOf("body:chore_dictionary:chore_walk:anim_files:05");
+
+            if (model.empty() || chore_prefix.empty() || chore_fidget.empty())
+            {
+                log->critical("super bad. something is empty on reading template={}", tmplName);
+                return;
+            }
+
+            if (mesh = static_cast<Aspect*>(osgDB::readNodeFile(model + ".asp")); mesh != nullptr)
+            {
+
+                transform = new osg::MatrixTransform;
+                transform->addChild(mesh);
+
+#if 1
+                // TODO: is there a better way to do this?
+                // re-position the camera based on the size of the mesh and orient it up a little bit get a birds eye-view
+                if (auto manipulator = viewer.getCameraManipulator())
+                {
+                    double radius = osg::maximum(double(mesh->getBound().radius()), 1e-6);
+                    double dist = 3.5f * radius;
+
+                    // farmgril is facing away from the camer so invert our distance - trying to keep this as close to the siege node test state
+                    manipulator->setHomePosition(mesh->getBound().center() + osg::Vec3d(0.0, -dist, 3.0f), mesh->getBound().center(), osg::Vec3d(0.0f, 0.0f, 1.0f));
+                    manipulator->home(1);
+                }
+#endif
+                // applying a skeleton messes with the bounding box size and expands it way more than it should
+                mesh->applySkeleton();
+
+                /* the below defines 9 stances for a template
+                   the running theory is that 00 is a default fourcc code
+                [chore_default]
+                {
+                    chore_stances = 0, 1, 2, 3, 4, 5, 6, 7, 8;
+                    skrit = simple_loop;
+                    [anim_files] { 00 = dff; }
+                }
+                */
+
+                // i think 00 gets read as an integer which ends up choping the leading 0
+                const auto fidgetFilename = chore_prefix + "0" + "_" + chore_fidget + ".prs";
+                const auto walkFilename = chore_prefix + "0" + "_" + chore_walk + ".prs";
+
+                log->info("retrieved '{}' from the contentdb. it has the model={}, chore_prefix={}, chore_fidget={}, fidgetFilename={} walkFilename={}", tmplName, model, chore_prefix, chore_fidget, fidgetFilename, walkFilename);
+
+                fidgetAnimation = static_cast<osgAnimation::Animation*>(osgDB::readObjectFile(fidgetFilename));
+                walkAnimation = static_cast<osgAnimation::Animation*>(osgDB::readObjectFile(walkFilename));
+
+                if (fidgetAnimation != nullptr && walkAnimation != nullptr)
+                {
+                    log->info("loaded fidget and walk animations for use and durations are {} and {} respectively", fidgetAnimation->getDuration(), walkAnimation->getDuration());
+
+                    animationManager = new osgAnimation::BasicAnimationManager;
+                    fidgetAnimation->setPlayMode(osgAnimation::Animation::LOOP);
+                    walkAnimation->setPlayMode(osgAnimation::Animation::LOOP);
+
+                    animationManager->registerAnimation(fidgetAnimation);
+                    animationManager->registerAnimation(walkAnimation);
+
+                    // start our fidget
+                    animationManager->playAnimation(fidgetAnimation);
+
+                    mesh->setUpdateCallback(animationManager);
+                }
+                else
+                {
+                    log->critical("failed to load a fidget or walk animation");
+                }
+            }
+        }
+        else
+        {
+            log->error("failed to retrieve {} from the contentdb", tmplName);
+        }
+
+    }
+
     void UnveilingTestState::enter()
     {
         log = spdlog::get("log");
@@ -135,105 +229,17 @@ namespace ehb
             }
         }
 
-        if (auto farmgirl = contentDb.getGameObjectTmpl("farmgirl")) 
+        localPlayer.reset(new Player(contentDb, viewer));
+
+        SiegePos position(0, 0, 0, targetNodeGuid);
+        if (auto localNode = region->transformForGuid(position.guid); localNode != nullptr)
         {
-            if (auto mesh = dynamic_cast<Aspect*>(osgDB::readNodeFile(farmgirl->valueOf("aspect:model") + ".asp")); mesh != nullptr)
-            {
-                mesh->applySkeleton();
-
-                SiegePos position(0, 0, 0, targetNodeGuid);
-
-                osg::ref_ptr<osg::MatrixTransform> transform = new osg::MatrixTransform;
-                transform->addChild(mesh);
-
-                if (auto localNode = region->transformForGuid(position.guid); localNode != nullptr)
-                {
-                    osg::Matrix copy = localNode->getMatrix();
-                    copy.preMultTranslate(position.pos);
-                    transform->setMatrix(copy);
-                }
-                else
-                {
-                    log->critical("failed to find the local transformation for targetNode");
-                }
-
-                region->addChild(transform);
-            }
-            else
-            {
-                log->critical("failed to find asp model for farmgirl");
-            }
-        }
-        else
-        {
-            log->critical("failed to find farmgirl template");
+            osg::Matrix copy = localNode->getMatrix();
+            copy.preMultTranslate(position.pos);
+            localPlayer->transform->setMatrix(copy);
         }
 
-        // collect all files under our object path for loading
-#if 0
-        for (const auto& filename : { "non_interactive.gas", "actor.gas" })
-        {
-            if (auto stream = fileSys.createInputStream(objectsPath + filename))
-            {
-                if (Fuel doc; doc.load(*stream))
-                {
-                    log->info("loading objects from: {}", objectsPath + filename);
-
-                    for (const auto& node : doc.eachChild())
-                    {
-                        const auto tmpl = node->type();
-                        const auto scid = node->name();
-
-                        log->debug("loading {} with scid {}", tmpl, scid);
-
-                        if (auto placement = node->child("placement"))
-                        {
-                            auto position = valueAsSiegePos(placement->valueOf("position"));
-
-                            auto go = contentDb.getGameObjectTmpl(tmpl);
-
-                            auto model = go->valueOf("aspect:model") + ".asp";
-
-                            if (auto mesh = dynamic_cast<Aspect*>(osgDB::readNodeFile(model)); mesh != nullptr)
-                            {
-                                // applying skeletons causes the update thread to run away. this needs to be looked at
-                                // mesh->applySkeleton();
-
-                                auto transform = new osg::MatrixTransform;
-
-                                transform->addChild(mesh);
-
-                                if (auto localNode = region->transformForGuid(position.guid); localNode != nullptr)
-                                {
-                                    osg::Matrix copy = localNode->getMatrix();
-                                    copy.preMultTranslate(position.pos);
-                                    transform->setMatrix(copy);
-                                }
-                                else
-                                {
-                                    log->debug("failed to find local node for {}", tmpl);
-                                }
-
-                                region->addChild(transform);
-                            }
-                            else
-                            {
-                                log->error("unable to load model: {}", model);
-                            }
-                        }
-                        else
-                        {
-                            log->debug("{} has no placement block - CRITICAL", tmpl);
-                        }
-                    }
-                }
-            }
-            else
-            {
-                log->error("failed to load an object file under path: {}", objectsPath + filename);
-            }
-        }
-#endif
+        region->addChild(localPlayer->transform);
 
         osgUtil::Optimizer optimizer;
         optimizer.optimize(region);
@@ -275,18 +281,6 @@ namespace ehb
                                     if (auto nodeXform = dynamic_cast<osg::MatrixTransform*> (node))
                                     {
                                         log->info("node you clicked was: 0x{:x}", nodeGuid);
-
-                                        if (selectedSiegeNode != nullptr)
-                                        {
-                                            selectedSiegeNode->toggleBoundingBox();
-                                            selectedSiegeNode->toggleAllDoorLabels();
-                                        }
-
-                                        if (selectedSiegeNode = static_cast<SiegeNodeMesh*>(nodeXform->getChild(0)); selectedSiegeNode != nullptr)
-                                        {
-                                            selectedSiegeNode->toggleBoundingBox();
-                                            selectedSiegeNode->toggleAllDoorLabels();
-                                        }
                                     }
                                 }
                             }
--- src/state/test/UnveilingTestState.hpp
+++ src/state/test/UnveilingTestState.hpp
@@ -3,6 +3,10 @@
 
 #include "state/IGameState.hpp"
 
+#include <osg/MatrixTransform>
+#include <osgAnimation/Animation>
+#include <osgAnimation/BasicAnimationManager>
+
 #include <spdlog/spdlog.h>
 
 namespace osg
@@ -17,9 +21,35 @@ namespace osgViewer
 
 namespace ehb
 {
+    class Aspect;
+    class ContentDb;
+}
+
+namespace ehb
+{
+    struct Player
+    {
+        uint32_t networkId = -1; // assigned by the network
+        uint32_t goid = 0; // local to their own machine
+
+        const std::string tmplName = "farmgirl"; // we are all farmgirls for now
+
+        Aspect* mesh = nullptr;
+
+        bool isFidgeting;
+        osg::ref_ptr<osgAnimation::Animation> fidgetAnimation = nullptr, walkAnimation = nullptr;
+        osg::ref_ptr<osgAnimation::BasicAnimationManager> animationManager = new osgAnimation::BasicAnimationManager;
+
+        osg::ref_ptr<osg::MatrixTransform> transform = new osg::MatrixTransform;
+
+        explicit Player::Player(ContentDb& contentDb, osgViewer::Viewer& viewer);
+    };
+}
+
+namespace ehb
+{
     class IConfig;
     class IFileSys;
-    class ContentDb;
     class Region;
     class SiegeNodeMesh;
     class UnveilingTestState: public IGameState
@@ -46,7 +76,9 @@ namespace ehb
         osg::Group& scene;
 
         Region* region;
-        SiegeNodeMesh* selectedSiegeNode = nullptr;
+
+        std::unique_ptr<Player> localPlayer = nullptr;
+        std::vector<std::unique_ptr<Player>> remotePlayers;
         
         std::shared_ptr<spdlog::logger> log;
     };
