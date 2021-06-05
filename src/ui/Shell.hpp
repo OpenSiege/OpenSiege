08f477e0a2df7e8204a39ff3175073f836a75383
OpenSiege <opensiege@gmail.com>
2021-06-05 15:01:04 -0600

    some basic interface functionality for the shell

--- src/ui/Shell.hpp
+++ src/ui/Shell.hpp
@@ -1,10 +1,52 @@
 
 #pragma once
 
+#include <stdint.h>
+
+namespace osg
+{
+    class Group;
+}
+
+namespace osgGA
+{
+    class GUIActionAdapter;
+    class GUIEventAdapter;
+}
+
 namespace ehb
 {
+    class IFileSys;
     class Shell
     {
+    public:
 
+        void init(IFileSys& fileSys, osg::Group* root, uint32_t width, uint32_t height);
+
+        bool handle(const osgGA::GUIEventAdapter& event, osgGA::GUIActionAdapter& action);
+
+        int32_t mouseX() const;
+        int32_t mouseY() const;
+
+        uint32_t screenWidth() const;
+        uint32_t screenHeight() const;
+
+    private:
+
+        // observer pointer?
+        osg::Group* scene2d = nullptr;
+
+        struct { int32_t x, y; } mouse;
+        struct { uint32_t width, height; } screen;
     };
+
+    inline int32_t Shell::mouseX() const
+    {
+        return mouse.x;
+    }
+
+    inline int32_t Shell::mouseY() const
+    {
+        return mouse.y;
+    }
 }
