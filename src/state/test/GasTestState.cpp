c83c63c3fb6be9ab8be3afabf1cd412723687ba5
OpenSiege <opensiege@gmail.com>
2021-08-13 17:37:18 -0600

    added a quick test for parsing 4 values from an attribute

--- src/state/test/GasTestState.cpp
+++ src/state/test/GasTestState.cpp
@@ -23,6 +23,30 @@ namespace ehb
             assert(doc.child("actor")->name() == "actor");
             assert(doc.child("actor")->type() == "template");
         }
+
+        std::stringstream().swap(*stream);
+        *stream << R"(
+            [t:test,n:readValues]
+            {
+                [testValueAsInt4]
+                {
+                    test = 1, 9, 6, 2;
+                }
+            }
+        )";
+
+        if (Fuel doc; doc.load(*stream))
+        {
+            auto readValuesBlock = doc.child("readValues");
+            assert(readValuesBlock != nullptr);
+            assert(readValuesBlock->name() == "readValues");
+
+            auto testValueAsInt4Block = readValuesBlock->child("testValueAsInt4");
+            assert(testValueAsInt4Block != nullptr);
+
+            auto testValuesAsInt4 = testValueAsInt4Block->valueAsInt4("test");
+            assert(testValuesAsInt4[0] == 1); assert(testValuesAsInt4[1] == 9); assert(testValuesAsInt4[2] == 6); assert(testValuesAsInt4[3] == 2);
+        }
     }
 
     void GasTestState::leave()
