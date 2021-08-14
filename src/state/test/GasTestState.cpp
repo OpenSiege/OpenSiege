134fba53ebd9668ee9041eab713f169030d57273
OpenSiege <opensiege@gmail.com>
2021-08-13 20:41:49 -0600

    added doctest just for some proper unit testing exception stuff

--- src/state/test/GasTestState.cpp
+++ src/state/test/GasTestState.cpp
@@ -8,12 +8,49 @@
 #include "IFileSys.hpp"
 #include "gas/Fuel.hpp"
 
+#define DOCTEST_CONFIG_IMPLEMENT
+#define DOCTEST_CONFIG_SUPER_FAST_ASSERTS
+#include "doctest.h"
+
 namespace ehb
 {
+
+    static void handler(const doctest::AssertData& ad)
+    {
+        using namespace doctest;
+
+        auto log = spdlog::get("testing");
+
+        std::stringstream ss;
+
+        ss << skipPathFromFilename(ad.m_file) << "(" << ad.m_line << "): " << failureString(ad.m_at) << ": ";
+
+        // handling only normal (comparison and unary) asserts - exceptions-related asserts have been skipped
+        if (ad.m_at & assertType::is_normal) {
+            ss << assertString(ad.m_at) << "( " << ad.m_expr << " ) ";
+            ss << (ad.m_threw ? "THREW exception: " : "is NOT correct!\n");
+            if (ad.m_threw)
+                ss << ad.m_exception;
+            else
+                ss << "  values: " << assertString(ad.m_at) << "( " << ad.m_decomp << " )";
+        }
+        else {
+            ss << "an assert dealing with exceptions has failed!";
+        }
+
+        log->error("{}", ss.str());
+    }
+
     void GasTestState::enter()
     {
         auto log = spdlog::get("log");
         log->info("GasTestState::enter()");
+
+        doctest::Context context;
+        context.setAsDefaultForAssertsOutOfTestCases();
+        context.setAssertHandler(handler);
+
+        CHECK_EQ(true, false);
 
         auto stream = std::make_unique<std::stringstream>();
         *stream << "[t:template,n:actor]{doc=\"Generic brained objects\";}";
